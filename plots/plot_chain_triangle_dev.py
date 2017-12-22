3ef7e10b2e83debac56033799cc7ae5798dfba80
Ajith Parameswaran <ajith.parameswaran@ligo.org>
2017-12-22 19:55:12 +0530

    minor cosmetics

--- plots/plot_chain_triangle_dev.py
+++ plots/plot_chain_triangle_dev.py
@@ -4,8 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import bayesian as ba
 import imrtestgr as tgr
+import plotsettings 
 
-data=np.genfromtxt('data_mod_gr.txt',delimiter=' ')
+model = 'gr'
+model = 'mod_gr'
+data=np.genfromtxt('data_%s.txt'% model, delimiter=' ')
 
 delta_mc_2=data[:,0] # delta mc in 2 independent variable case
 delta_q_2=data[:,1]  # delta q  in 2 independent variable case
@@ -15,7 +18,7 @@ delta_q_1=data[:,3]  # delta q  in 1 independent variable case
 #######
 #1d-histograms of delta mc ,delta q - 1 independent variable case 
 #######
-N_bins = 35
+N_bins = 50
 dmc_min = min(delta_mc_1)
 dmc_max = max(delta_mc_1)
 dq_min = min(delta_q_1)
@@ -58,7 +61,7 @@ s1 = ba.nsigma_value(P_dmc_dq_2, 0.68) # 1 sigma level of 2d histogram of delta 
 s2 = ba.nsigma_value(P_dmc_dq_2, 0.95) # 2 sigma level of 2d histogram of delta mc ,delta q - 2 independent variable case
 
 
-plt.figure()
+plt.figure(figsize=(6,5))
 plt.subplot2grid((3,3), (0,0), colspan=2)
 plt.plot(dmc_edges_1[:-1],dmc_hist_1,'b', lw=1)
 plt.plot(dmc_edges_2[:-1],dmc_hist_2,'k', lw=1)
@@ -66,16 +69,19 @@ plt.axvline(0.,color='k', lw=0.6,linestyle='-.')
 plt.ylabel('$P(\Delta M_c)$',fontsize=14)
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
+plt.xlim(-0.2,0.2)
 
 
 plt.subplot2grid((3,3), (1,0), colspan=2, rowspan=2)
 plt.pcolormesh(dmc_bins_2[:-1],dq_bins_2[:-1],P_dmc_dq_2, cmap='YlOrBr')
 plt.contour(dmc_bins_2[:-1],dq_bins_2[:-1],tgr.gf(P_dmc_dq_2), levels=(s2,s1), linewidths=(1,1.5))
-plt.plot(0, 0, 'w+', ms=12, mew=2) # for mod gr make the marker k+ , for gr make the marker w+
+plt.plot(0, 0, 'k+', ms=12, mew=2) # for mod gr make the marker k+ , for gr make the marker w+
 plt.xlabel('$\Delta M_c (M_\odot)$',fontsize=14)
 plt.ylabel('$\Delta q$',fontsize=14)
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
+plt.xlim(-0.2,0.2)
+plt.ylim(-1e-3,1e-3)
 
 
 plt.subplot2grid((3,3), (1,2), rowspan=2)
@@ -84,9 +90,10 @@ plt.plot(dq_hist_1, dq_edges_1[:-1],'b', lw=1)
 plt.axhline(0.,color='k', lw=0.6,linestyle='-.')
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
+plt.ylim(-1e-3,1e-3)
 plt.xlabel('$P(\Delta q)$',fontsize=14)
 
 plt.tight_layout()
-plt.savefig('triangle_plot_gr.png')
+plt.savefig('triangle_plot_%s.png' %model, dpi=300)
 plt.close()
 

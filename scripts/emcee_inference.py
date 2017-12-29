e8e042ed6863effd81d84c9e071be9c9b11490f4
Siddharth Dhanpal <dhanpal.siddharth@cbs.ac.in>
2017-12-29 03:40:49 -0500

    Modified in cos i

--- scripts/emcee_inference.py
+++ scripts/emcee_inference.py
@@ -14,7 +14,7 @@ import corner
 
 
 
-def lnlike(param_vec, dr, di, freq, psd):
+def lnlike(param_vec, dr, di, freq, psd, f_low, f_cut):
 	"""
 	compute the log likelihood
 	
@@ -23,35 +23,42 @@ def lnlike(param_vec, dr, di, freq, psd):
 	dr, di, 
 	freq : Fourier freq 
 	psd : psd vector 
-
+	flow,fcut
+	
 	output: 
 	log_likelhood 
 	"""
+	df = np.mean(np.diff(freq))
+
+        N_low=np.int((f_low-freq[0])/df)
+        N_cut=np.int((f_cut-freq[0])/df)
+
+        Nls=np.int(f_low/df)  #N_low_signal
+        Ncs=np.int(f_cut/df)  #N_cut_signal
 	
 	# unpacking the parameter vector 
-	M, q, dL, iota, t0, phase = param_vec	
+	Mc, q, dL, ci, t0, phi_0 = param_vec	
 
 	# generate the waveform 
-	f, hpf, hcf = phhsi.phenomhh_waveform_SI(M, q, dL, iota, t0, phase, freq[200], 0.1)
+	f, hpf, hcf = phhsi.phenomhh_waveform_SI(Mc, q, dL, np.arccos(ci), t0, phi_0, flow, df, Ncs)
+
 	ra=1.
 	dec =1.
 	pol=0.
-
 	# compute antenna patterns 
 	Fp,Fc = detector.overhead_antenna_pattern(ra, dec, pol)	
-	signal=Fp*hpf+Fc*hcf
-	signalr=np.real(signal)
-	signali=np.imag(signal)
 
-	like_list=-((dr[200:9998]-signalr[200:9998])*(dr[200:9998]-signalr[200:9998])+(di[200:9998]-signali[200:9998])*(di[200:9998]-signali[200:9998]))/(psd[200:9998]) #likelihood list	
-	like= 0.2*np.sum(like_list)#0.2 = 0.5*4.*df from the inner-product definition
+	signal=Fp*hpf+Fc*hcf
+
+	like = -2.*df*np.real(np.dot(data[N_low:N_cut]-signal[Nls:Ncs],np.conj((data[N_low:N_cut]-signal[Nls:Ncs])/psd[N_low:N_cut])))
+
 	return like#log-likelihood
 
 
 def lnprior(param_vec):
-	M,q,dL,iota,t0,phase = param_vec
-	if 1 < M < 200 and 0.097 < q <= 1. and 1.<dL<10000 and 0. <= iota <= pi and 0.<= t0 <= 15. and 0. <= phase <= 2.*pi:
-		return 0.0
+	Mc, q, dL, ci, t0, phi_0 = param_vec
+	if 1 < Mc < 200 and 0.05 < q <= 1. and 1.<dL<10000 and -1. < ci <= 1. and 0.<= t0 <= 15. and 0. <= phi_0 <= 2.*pi:
+		return 2.*np.log(dL)
 	return -np.inf
 
 
@@ -73,11 +80,14 @@ loc='/home/siddharth.dhanpal/Work/projects/imrtestgr_hh/runs/201708_pe_results_p
 loc = '/home/ajith/working/cbc/gr_consistency_highermodes/runs/201708_pe_results_phenomhh/m_70_q_5_i_90/22_plus_HM/data'
 loc = '/home/siddharth.dhanpal/Work/projects/imrtestgr_hh/runs/experiments_201709/M_80/q_9/i_60/data'
 loc = '/home/ajith/working/cbc/gr_consistency_highermodes/runs/experiments_201709/M_80/q_9/i_60/data'
-result=[21.4140366758,1./5,198.717477328,pi/2,6.,pi]#initial guess around which walkers start. This is also true value.Mc,q,dL,i,t0,initial_phase 
+input_loc  = 
+output_loc = 
+
+result=[21.4140366758,1./5,198.717477328,np.cos(pi/2),6.,pi]#initial guess around which walkers start. This is also true value.Mc,q,dL,i,t0,initial_phase 
 data_fname = 'detected_data.txt'
 
 # labels of the parameter vector 
-param_label = ['$M_c$', '$q$', '$dL$', '$\iota$', '$t_0$', '$\phi_0$']
+param_label = ['$M_c$', '$q$', '$dL$', 'cos($\iota$)', '$t_0$', '$\phi_0$']
 
 
 ndim, nwalkers = 6, 100
@@ -111,7 +121,7 @@ pos = [result + np.array([0.002*result[0]*np.random.random_sample()-0.001*result
 
 # make corner plot of the initial walkers 
 plt.figure()
-figure = corner.corner(pos, label=param_label, bins=10)
+figure = corner.corner(pos, labels=param_label, bins=10)
 figure.tight_layout()
 figure.savefig('%s/initial_walkers_corner.png' %loc, dpi=200)    
 
@@ -127,4 +137,26 @@ for result in sampler.sample(pos, iterations=num_iter, storechain=False):
 		f.write("{0:1d} {1:2f} {2:3f} {3:4f} {4:5f} {5:6f} {6:7f}\n".format(k,p[0],p[1],p[2],p[3],p[4],p[5]))
 	f.close()
 
-# add the final corner plots here 
+# add the final corner plots here
+
+data=np.genfromtxt('%s/chain_file.dat'%loc,delimiter=' ')
+
+chain_length=int(len(data[:,0])/n_walkers)
+s=(nwalkers,chain_length,ndim)
+x=np.zeros(s)
+
+i=0
+for j in range(x.shape[1]):
+       	for l in range(x.shape[0]):
+		for k in range(x.shape[2]):
+	               	x[l][j][k]=data[i][k+1]
+               	i+=1
+
+
+samples = x[:,:,:].reshape((-1, ndim))
+
+plt.figure()
+corner.corner(samples, labels=param_label)
+plt.savefig("%s/final_corner_plot_without_burnin.png"%loc_plots)
+plt.close()
+ 

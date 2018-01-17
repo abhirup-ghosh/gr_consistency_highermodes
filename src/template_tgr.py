727be6f7687e51f31a34fbe98a97c00902cfedb7
Siddharth Dhanpal <dhanpal.siddharth@cbs.ac.in>
2018-01-16 22:29:34 -0500

    Added tgr template and modified other templates

--- src/template_tgr.py
+++ src/template_tgr.py
@@ -1,5 +1,5 @@
 """
-template for only (2,2) mode
+Template for test of GR in 2 variable case Mc+dMc, q+dq
 """
 
 from numpy import sqrt, sin, cos, pi,exp
@@ -9,16 +9,7 @@ import numpy as np
 #import matplotlib.pyplot as plt
 import phenomhh as phh
 from lal import MSUN_SI, MTSUN_SI, PC_SI, PI, PC_SI, C_SI, GAMMA, MRSUN_SI
-import pycbc.waveform as pw
-import pycbc.filter.matchedfilter as mfilter
-import pycbc.psd
-import pycbc.noise.gaussian
-from pycbc  import  detector
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import pycbc.types.frequencyseries
-from pycbc.types import TimeSeries, FrequencySeries, zeros
 
 
 def ringdown(Mc,q,l,m,Ncs,df):
@@ -50,30 +41,56 @@ def ringdown(Mc,q,l,m,Ncs,df):
 
 
 
-def phenomhh_waveform_SI(Mc,q,r,iota,t0,phase,f_low,df,Ncs):
 
-	N=ringdown(Mc,q,2,2,Ncs,df)
+
+def phenomhh_waveform_SI(Mc,q,Mc1,q1,r,iota,t0,phase,f_low,df,Ncs):
+
+	N = np.max(np.array([ringdown(Mc,q,2,2,Ncs,df),ringdown(Mc1,q1,2,1,Ncs,df),ringdown(Mc1,q1,3,3,Ncs,df),ringdown(Mc1,q1,4,4,Ncs,df)]))
+        N = np.int(N)
 
 	M=((1.+q)**1.2)*Mc/(q**0.6)
 	m1=M/(q+1.)
 	m2=M*q/(q+1.)
 	m1=m1*MSUN_SI 
 	m2=m2*MSUN_SI
-	mt=m1+m2 
-	incl_angle=iota 
+	mt=m1+m2 # mtotal
+
+
+        M1=((1.+q1)**1.2)*Mc1/(q1**0.6)
+        m1_1=M1/(q1+1.)
+        m2_2=M1*q1/(q1+1.)
+        m1_1=m1_1*MSUN_SI 
+        m2_2=m2_2*MSUN_SI
+        mt_1=m1_1+m2_2 # mtotal
+
+	incl_angle=iota # inclination angle
 	phi=0. 
 	lmax=4
-	Psi_ref=phase
-	
+
+	Psi_ref = phase #initial phase	
+
 	hpf22,hcf22 = phh.generate_phenomhmv1_fd(m1, m2, incl_angle, phi, f_low, df, N, lmax,[[2,2]], Psi_ref) 
+	hpf21,hcf21 = phh.generate_phenomhmv1_fd(m1_1, m2_2, incl_angle, phi, f_low, df, N, lmax,[[2,1]], Psi_ref)
+	hpf33,hcf33 = phh.generate_phenomhmv1_fd(m1_1, m2_2, incl_angle, phi, f_low, df, N, lmax,[[3,3]], Psi_ref)
+	hpf44,hcf44 = phh.generate_phenomhmv1_fd(m1_1, m2_2, incl_angle, phi, f_low, df, N, lmax,[[4,4]], Psi_ref)
 	
 	f=np.linspace(0., df*(N-1), N)
 	
 	hpf22=hpf22*mt*MRSUN_SI*MTSUN_SI*mt*exp(-2*pi*1j*f*t0)/(MSUN_SI*MSUN_SI*(1.0e6*r*PC_SI))
 	hcf22=hcf22*mt*MRSUN_SI*MTSUN_SI*mt*exp(-2*pi*1j*f*t0)/(MSUN_SI*MSUN_SI*(1.0e6*r*PC_SI))
 
-	hpf=hpf22
-	hcf=hcf22
+        hpf21=hpf21*mt_1*MRSUN_SI*MTSUN_SI*mt_1*exp(-2*pi*1j*f*t0)/(MSUN_SI*MSUN_SI*(1.0e6*r*PC_SI))
+        hcf21=hcf21*mt_1*MRSUN_SI*MTSUN_SI*mt_1*exp(-2*pi*1j*f*t0)/(MSUN_SI*MSUN_SI*(1.0e6*r*PC_SI))
+
+        hpf33=hpf33*mt_1*MRSUN_SI*MTSUN_SI*mt_1*exp(-2*pi*1j*f*t0)/(MSUN_SI*MSUN_SI*(1.0e6*r*PC_SI))
+        hcf33=hcf33*mt_1*MRSUN_SI*MTSUN_SI*mt_1*exp(-2*pi*1j*f*t0)/(MSUN_SI*MSUN_SI*(1.0e6*r*PC_SI))
+
+        hpf44=hpf44*mt_1*MRSUN_SI*MTSUN_SI*mt_1*exp(-2*pi*1j*f*t0)/(MSUN_SI*MSUN_SI*(1.0e6*r*PC_SI))
+        hcf44=hcf44*mt_1*MRSUN_SI*MTSUN_SI*mt_1*exp(-2*pi*1j*f*t0)/(MSUN_SI*MSUN_SI*(1.0e6*r*PC_SI))
+
+
+	hpf=hpf22+hpf21+hpf33+hpf44
+	hcf=hcf22+hcf21+hcf33+hcf44
 
 
 	return f,hpf,hcf

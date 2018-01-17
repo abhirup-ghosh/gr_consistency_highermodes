5066e3150250c227fdc24c77b610c8d06ceb76c2
Siddharth Dhanpal <dhanpal.siddharth@cbs.ac.in>
2018-01-16 22:28:24 -0500

    Added tgr script of 2 var

--- scripts/emcee_inference_tgr.py
+++ scripts/emcee_inference_tgr.py
@@ -6,7 +6,7 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import emcee
-import template_hm as phhsi
+import template_tgr as phhsi
 from pycbc  import  detector
 from lal import MSUN_SI, MTSUN_SI, PC_SI, PI, PC_SI, C_SI, GAMMA, MRSUN_SI
 import corner
@@ -37,10 +37,10 @@ def lnlike(param_vec, data, freq, psd, f_low, f_cut):
         Ncs=np.int(f_cut/df)  #N_cut_signal
 	
 	# unpacking the parameter vector 
-	Mc, q, dL, ci, t0, phi_0 = param_vec	
+	Mc, q, Mc1, q1, dL, i, t0, phi_0 = param_vec	
 
 	# generate the waveform 
-	f, hpf, hcf = phhsi.phenomhh_waveform_SI(Mc, q, dL, np.arccos(ci), t0, phi_0, f_low, df, Ncs)
+	f, hpf, hcf = phhsi.phenomhh_waveform_SI(Mc, q, Mc1, q1, dL, i, t0, (phi_0 %(2.*pi)), f_low, df, Ncs)
 
 
 	ra=1.
@@ -57,9 +57,9 @@ def lnlike(param_vec, data, freq, psd, f_low, f_cut):
 
 
 def lnprior(param_vec):
-	Mc, q, dL, ci, t0, phi_0 = param_vec
-	if 1 < Mc < 200 and 0.05 < q <= 1. and 1.<dL<10000 and -1. < ci <= 1. and 0.<= t0 <= 15. and 0. <= phi_0 <= 2.*pi:
-		return 2.*np.log(dL)
+	Mc, q, Mc1, q1, dL, i, t0, phi_0 = param_vec
+	if 1 < Mc < 200 and 0.05 < q <= 1. and  1 < Mc1 < 200 and 0.05 < q1 <= 1. and 1.<dL<10000 and 0.<= i <= pi and 0.<= t0 <= 15. and -pi <= phi_0 <= 3.*pi:
+		return 0.0
 	return -np.inf
 
 
@@ -77,25 +77,20 @@ def lnprob(param_vec):
 
 
 # -------------------- inputs -------------------------- # 
-loc='/home/siddharth.dhanpal/Work/projects/imrtestgr_hh/runs/201708_pe_results_phenomhh/m_70_q_5_i_90/22_plus_HM/data'
-loc = '/home/ajith/working/cbc/gr_consistency_highermodes/runs/201708_pe_results_phenomhh/m_70_q_5_i_90/22_plus_HM/data'
-loc = '/home/siddharth.dhanpal/Work/projects/imrtestgr_hh/runs/experiments_201709/M_80/q_9/i_60/data'
-loc = '/home/ajith/working/cbc/gr_consistency_highermodes/runs/experiments_201709/M_80/q_9/i_60/data'
-loc = '/home/siddharth.dhanpal/Work/projects/imrtestgr_hh/runs/201711_pe_results_noise_free_analysis/M_70/q_9/i_60/data'
-loc = '/home/siddharth.dhanpal/Work/projects/imrtestgr_hh/scripts/final_scripts'
+loc = '/home/siddharth.dhanpal/Work/projects/imrtestgr_hh/scripts/final_scripts'#'/home/siddharth.dhanpal/Work/projects/imrtestgr_hh/runs/201711_pe_deviations_noise_free_analysis/M_80/q_9/i_60/data'
 
-result=[ 1.886407405431894801e+01, 1./9, 3.885969673824694723e+02, np.cos(pi/3), 6., pi]#result=[16.4140366758,1./9,198.717477328,np.cos(pi/2),6.,pi]#[21.4140366758,1./5,198.717477328,np.cos(pi/2),6.,pi]#initial guess around which walkers start. This is also true value.Mc,q,dL,i,t0,initial_phase 
+result=[ 1.886407405431894801e+01, 1./9, 1.886407405431894801e+01, 1./9, 3.885969673824694723e+02, pi/3, 6., pi]#initial guess around which walkers start. This is also true value.Mc,q,dL,i,t0,initial_phase for quicker convergence. *If any injection value is 0 add 0.01 to it. 
 data_fname = 'detected_data.txt'
 
 # labels of the parameter vector 
-param_label = ['$M_c$', '$q$', '$dL$', 'cos($\iota$)', '$t_0$', '$\phi_0$']
+param_label = ['$M_c$', '$q$', '$M_c_1$', '$q_1$' ,'$dL$', '$\iota$', '$t_0$', '$\phi_0$']
 
 f_low = 20.
 f_cut = 999.
 
-ndim, nwalkers = 6, 100
+ndim, nwalkers = 8, 100
 num_threads = 24 
-num_iter = 2500 
+num_iter = 3000 
 # ------------------------------------------------------ # 
 
 
@@ -104,9 +99,34 @@ freq, dr, di, psd = np.loadtxt(loc+'/'+data_fname, unpack=True)
 data = dr + 1j*di 
 print '... read data' 
 
+# create initial walkers 
+pos = [result + np.array([0.0002*result[0]*np.random.random_sample()-0.0001*result[0],0.002*result[1]*np.random.random_sample()-0.001*result[1],0.0002*result[2]*np.random.random_sample()-0.0001*result[2],0.002*result[3]*np.random.random_sample()-0.001*result[3],0.002*result[4]*np.random.random_sample()-0.001*result[4],0.002*result[5]*np.random.random_sample()-0.001*result[5],0.0002*result[6]*np.random.random_sample()-0.0001*result[6],0.002*result[7]*np.random.random_sample()-0.001*result[7]]) for i in range(nwalkers)]
+
+##make corner plot of the initial walkers 
+#plt.figure()
+#figure = corner.corner(pos, labels=param_label, bins=10)
+#figure.tight_layout()
+#figure.savefig('%s/initial_walkers_corner.png' %loc, dpi=200)    
+
+print '... generated initial walkers. starting sampling...' 
+
+# sample the likelihood using EMCEE 
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, threads=num_threads)
+
+# writing data for each itteration.
+for result in sampler.sample(pos, iterations=num_iter, storechain=False):
+	position = result[0]
+	f = open("%s/chain_tgr_2_var.dat"%loc, "a")
+	for k in range(position.shape[0]):
+		p=position[k]
+		f.write("{0:1d} {1:2f} {2:3f} {3:4f} {4:5f} {5:6f} {6:7f} {7:8f}{8:9f}\n".format(k,p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7]%(2.*pi)))# Order: walker number, Mc, q, Mc1, q1, dL, iota, t0, phi_0 
+	f.close()
+
+
+
 # plot the data and the psd 
 df = np.mean(np.diff(freq))
-idx = np.logical_and(freq > 20, freq < 500)
+idx = np.logical_and(freq > 20, freq < 999)
 snr = 2*np.sqrt(df*np.sum(abs(data[idx])**2/psd[idx]))
 
 plt.figure(figsize=(8,6))
@@ -119,33 +139,10 @@ plt.ylabel('$h(f)$ and $S_h(f)$')
 plt.title('snr = %2.1f' %snr)
 plt.savefig('%s/data.png'%loc, dpi=200)
 
-print '... plotted data' 
-
-# create initial walkers 
-pos = [result + np.array([0.002*result[0]*np.random.random_sample()-0.001*result[0],0.002*result[1]*np.random.random_sample()-0.001*result[1],0.002*result[2]*np.random.random_sample()-0.001*result[2],0.002*result[3]*np.random.random_sample()-0.001*result[3],0.002*result[4]*np.random.random_sample()-0.001*result[4],0.002*result[5]*np.random.random_sample()-0.001*result[5]]) for i in range(nwalkers)]
-
-# make corner plot of the initial walkers 
-plt.figure()
-figure = corner.corner(pos, labels=param_label, bins=10)
-figure.tight_layout()
-figure.savefig('%s/initial_walkers_corner.png' %loc, dpi=200)    
-
-print '... generated initial walkers. starting sampling...' 
-
-# sample the likelihood using EMCEE 
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, threads=num_threads)
-
-# writing data for each itteration.
-for result in sampler.sample(pos, iterations=num_iter, storechain=False):
-	position = result[0]
-	f = open("%s/chain_hm_plus_22.dat"%loc, "a")
-	for k in range(position.shape[0]):
-		p=position[k]
-		f.write("{0:1d} {1:2f} {2:3f} {3:4f} {4:5f} {5:6f} {6:7f}\n".format(k,p[0],p[1],p[2],p[3],p[4],p[5]))
-	f.close()
+print '... plotted data'
 
 # add the final corner plots here
-chain_data=np.genfromtxt('%s/chain_hm_plus_22.dat'%loc,delimiter=' ')
+chain_data=np.genfromtxt('%s/chain_tgr_2_var.dat'%loc,delimiter=' ')
 
 chain_length=int(len(chain_data[:,0])/n_walkers)
 s=(nwalkers,chain_length,ndim)
@@ -160,8 +157,11 @@ for j in range(x.shape[1]):
 
 samples = x[:,:,:].reshape((-1, ndim))
 
+
 plt.figure()
 corner.corner(samples, labels=param_label)
 plt.savefig("%s/final_corner_plot_without_burnin.png"%loc)
 plt.close()
- 
+
+print '... plotted corner plot' 
+

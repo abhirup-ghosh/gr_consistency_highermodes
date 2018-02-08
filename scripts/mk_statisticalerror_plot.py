83263c292fdcc6874a9a8c12959a3022a41d29b9
Ajith Parameswaran <ajith.parameswaran@ligo.org>
2018-02-08 18:12:10 +0530

    trying to plot delta_Mc and delta_q errors in the same plot

--- scripts/mk_statisticalerror_plot.py
+++ scripts/mk_statisticalerror_plot.py
@@ -2,8 +2,7 @@ import matplotlib as mpl
 #mpl.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
-#import plotsettings
-from optparse import OptionParser
+import plotsettings
 from matplotlib import rc
 mpl.rc('text.latex', preamble = '\usepackage{txfonts}')
 import bayesian as ba
@@ -24,22 +23,30 @@ rc_params = {'backend': 'ps',
              'font.sans-serif': ['Bitstream Vera Sans']
              }
 
-color = ['#0072b2', '#CC79A7']
 
-post_loc = '../data/90_percent_width_dMc_t.txt'
-q, i_30, i_45, i_60, i_80, i_90 = np.loadtxt(post_loc, unpack=True)
+fname_vec = ['../data/90_percent_width_dMc_t_abhi.txt', '../data/90_percent_width_dq_t_abhi.txt']
 
 plt.figure(figsize=(5,5))
-ax = plt.subplot(111)
-ax.semilogy(q, i_30, color='r', marker='o', label='$\iota = 30^{\circ}$')
-ax.semilogy(q, i_45, color='orange', marker='o', label='$\iota = 45^{\circ}$')
-ax.semilogy(q, i_60, color='g', marker='o', label='$\iota = 60^{\circ}$')
-ax.semilogy(q, i_80, color='b', marker='o', label='$\iota = 80^{\circ}$')
-ax.semilogy(q, i_90, color='k', marker='o', label='$\iota = 90^{\circ}$')
-plt.legend(loc='best', frameon=False, fontsize=12)
-ax.set_xlabel('Asymmetric Mass Ratio (q)', labelpad=10, fontsize=12)
-ax.set_ylabel('90% width of $\Delta M_c/M_c$', labelpad=10, fontsize=12)
+
+for i, fname in enumerate(fname_vec):
+
+	q, i_30, i_45, i_60, i_80, i_90 = np.loadtxt(fname, unpack=True)
+
+	ax = plt.subplot(2,1,i+1)
+	ax.semilogy(q, i_30, color='r', marker='^', label='$\iota = 30^{\circ}$')
+	ax.semilogy(q, i_45, color='orange', marker='s', label='$\iota = 45^{\circ}$')
+	ax.semilogy(q, i_60, color='lawngreen', marker='o', label='$\iota = 60^{\circ}$')
+	ax.semilogy(q, i_80, color='cyan', marker='d', label='$\iota = 80^{\circ}$')
+	ax.semilogy(q, i_90, color='blue', marker='v', label='$\iota = 90^{\circ}$')
+	plt.legend(loc='best', frameon=False, fontsize=12)
+
+	if i == 0:
+		ax.set_ylabel('90\% width of $\Delta M_c/M_c$', labelpad=10, fontsize=12)
+	else: 
+		ax.set_ylabel('90\% width of $\Delta q$', labelpad=10, fontsize=12)
+
+ax.set_xlabel('Mass ratio $q$', labelpad=10, fontsize=12)
 plt.tight_layout()
 plt.savefig('../papers/intro_paper/figs/fig3.png', dpi=300)
 plt.savefig('../papers/intro_paper/figs/fig3.pdf')
-plt.show()
+plt.close()

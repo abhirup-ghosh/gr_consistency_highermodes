0a018d8ef0bb93b50cb51481ef01c9d16701b7f5
Ajith Parameswaran <ajith.parameswaran@ligo.org>
2018-02-09 12:46:45 +0530

    updated Fig 2 including the dMc and dq errors

--- scripts/mk_statisticalerror_plot.py
+++ scripts/mk_statisticalerror_plot.py
@@ -10,42 +10,46 @@ import imrtestgr as tgr
 import scipy
 from scipy import interpolate
 
-# Make the font match the document
-rc_params = {'backend': 'ps',
-             'axes.labelsize': 10,
-             'axes.titlesize': 10,
-             'font.size': 12,
-             'legend.fontsize': 12,
-             'xtick.labelsize': 13,
-             'ytick.labelsize': 13,
-             'font.family': 'Times New Roman',
-             'font.family': 'sans-serif',
-             'font.sans-serif': ['Bitstream Vera Sans']
-             }
+# # Make the font match the document
+# rc_params = {'backend': 'ps',
+#              'axes.labelsize': 15,
+#              'axes.titlesize': 15,
+#              'font.size': 15,
+#              'legend.fontsize': 15,
+#              'xtick.labelsize': 15,
+#              'ytick.labelsize': 15,
+#              'font.family': 'Times New Roman',
+#              'font.family': 'sans-serif',
+#              'font.sans-serif': ['Bitstream Vera Sans']
+#              }
 
+col = 'k'
 
 fname_vec = ['../data/90_percent_width_dMc_t_abhi.txt', '../data/90_percent_width_dq_t_abhi.txt']
+fname_vec = ['../data/90_percent_width_dMc_t_sidharth.txt', '../data/90_percent_width_dq_t_sidharth.txt']
 
-plt.figure(figsize=(5,5))
+plt.figure(figsize=(4,4))
 
 for i, fname in enumerate(fname_vec):
 
 	q, i_30, i_45, i_60, i_80, i_90 = np.loadtxt(fname, unpack=True)
 
 	ax = plt.subplot(2,1,i+1)
-	ax.semilogy(q, i_30, color='r', marker='^', label='$\iota = 30^{\circ}$')
-	ax.semilogy(q, i_45, color='orange', marker='s', label='$\iota = 45^{\circ}$')
-	ax.semilogy(q, i_60, color='lawngreen', marker='o', label='$\iota = 60^{\circ}$')
-	ax.semilogy(q, i_80, color='cyan', marker='d', label='$\iota = 80^{\circ}$')
-	ax.semilogy(q, i_90, color='blue', marker='v', label='$\iota = 90^{\circ}$')
-	plt.legend(loc='best', frameon=False, fontsize=12)
+	ax.semilogy(q, i_30, color='lightgray', marker='^', label='$\iota = 30^{\circ}$', alpha=1)
+	ax.semilogy(q, i_45, color='lightpink', marker='s', label='$\iota = 45^{\circ}$', alpha=1)
+	ax.semilogy(q, i_60, color='crimson', marker='o', label='$\iota = 60^{\circ}$', alpha=1)
+	ax.semilogy(q, i_80, color='k', marker='d', label='$\iota = 80^{\circ}$', alpha=1, lw=1, ls='--')
+	ax.semilogy(q, i_90, color='deeppink', marker='v', label='$\iota = 90^{\circ}$', alpha=1, lw=1)
+	ax.set_ylim(1e-3,1e-1)
+	ax.set_xlim(2, 9)
 
 	if i == 0:
-		ax.set_ylabel('90\% width of $\Delta M_c/M_c$', labelpad=10, fontsize=12)
+		ax.set_ylabel('90\% width of $\Delta M_c/M_c$', labelpad=5, fontsize=10)
+		plt.legend(loc='best', frameon=False, fontsize=8)
 	else: 
-		ax.set_ylabel('90\% width of $\Delta q$', labelpad=10, fontsize=12)
+		ax.set_ylabel('90\% width of $\Delta q$', labelpad=7, fontsize=10)
 
-ax.set_xlabel('Mass ratio $q$', labelpad=10, fontsize=12)
+ax.set_xlabel('Mass ratio $q$', labelpad=5, fontsize=10)
 plt.tight_layout()
 plt.savefig('../papers/intro_paper/figs/fig3.png', dpi=300)
 plt.savefig('../papers/intro_paper/figs/fig3.pdf')

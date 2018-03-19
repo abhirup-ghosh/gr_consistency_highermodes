cee577f728c951dd71b2f49e4fb61c2e736736c2
abhirup.ghosh <abhirup.ghosh@ligo.org>
2018-03-19 12:42:41 +0530

    updated figure 3b

--- scripts/mk_statisticalerror_plot.py
+++ scripts/mk_statisticalerror_plot.py
@@ -1,7 +1,8 @@
 import matplotlib as mpl
 mpl.use('Agg')
-import numpy as np
+import os, sys, numpy as np
 import matplotlib.pyplot as plt
+sys.path.insert(0, '../src')
 import plotsettings
 from matplotlib import rc
 mpl.rc('text.latex', preamble = '\usepackage{txfonts}')
@@ -26,7 +27,7 @@ from scipy import interpolate
 col = 'k'
 
 fname_vec_q = ['../data/90_percent_width_9dim_DeltaMcbyMcinj_diffq_abhi.txt', '../data/90_percent_width_9dim_Deltaq_diffq_abhi.txt']
-fname_vec_M = ['../data/90_percent_width_9dim_DeltaMcbyMcinj_diffM_abhi.txt', '../data/90_percent_width_9dim_Deltaq_diffM_abhi.txt']
+fname_vec_M = ['../data/90_percent_width_9dim_DeltaMcbyMcinj_diffM_abhi.txt', '../data/90_percent_width_9dim_Deltaq_diffM_abhi_4000iternations.txt']
 
 plt.figure(figsize=(4,4))
 
@@ -52,8 +53,8 @@ for i, fname in enumerate(fname_vec_q):
 
 ax.set_xlabel('Mass ratio $q$', labelpad=5, fontsize=10)
 plt.tight_layout()
-plt.savefig('../papers/intro_paper/figs/fig3a_9dim_dmcbymcinj_dq_abhi.png', dpi=300)
-plt.savefig('../papers/intro_paper/figs/fig3a_9dim_dmcbymcinj_dq_abhi.pdf')
+#plt.savefig('../papers/intro_paper/figs/fig3a_9dim_dmcbymcinj_dq_abhi.png', dpi=300)
+#plt.savefig('../papers/intro_paper/figs/fig3a_9dim_dmcbymcinj_dq_abhi.pdf')
 plt.close()
 
 plt.figure(figsize=(4,4))
@@ -70,18 +71,17 @@ for i, fname in enumerate(fname_vec_M):
         ax.semilogy(M, i_90, color='deeppink', marker='v',  markeredgecolor='deeppink', label='$\iota = 90^{\circ}$', alpha=1, lw=1)
 
         if i == 0:
+		ax.set_xlim(40,200)
 		ax.set_ylim(1e-4,1e-2)
-		ax.set_ylabel('90\% width of $\Delta M_c/M_{c,inj}$', labelpad=5, fontsize=10)
+		ax.set_ylabel('90\% width of $\Delta M_c/M_c^{\mathrm{inj}}$', labelpad=5, fontsize=10)
 		plt.legend(loc='best', frameon=False, fontsize=8)
-		ax.set_xlim(min(M), max(M))
         else:
 		ax.set_ylim(1e-4, 1e-2)
-		ax.set_xlim(min(M), max(M))
 		ax.set_ylabel('90\% width of $\Delta q$', labelpad=7, fontsize=10)
+		ax.set_xlim(40,200)
 
 ax.set_xlabel('Total mass $M (M_{\odot})$', labelpad=5, fontsize=10)
-ax.set_xlim(min(M), max(M))
 plt.tight_layout()
-plt.savefig('../papers/intro_paper/figs/fig3b_9dim_dmcbymcinj_dq_abhi.png', dpi=300)
-plt.savefig('../papers/intro_paper/figs/fig3b_9dim_dmcbymcinj_dq_abhi.pdf')
+plt.savefig('../papers/intro_paper/figs/fig3b_9dim_dmcbymcinj_dq_abhi_4000iter.png', dpi=300)
+plt.savefig('../papers/intro_paper/figs/fig3b_9dim_dmcbymcinj_dq_abhi_4000iter.pdf')
 plt.close()

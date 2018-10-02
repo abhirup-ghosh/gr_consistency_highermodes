e9b900276ad63a7d56cd641b671307322a91c110
abhirup.ghosh <abhirup.ghosh@ligo.org>
2018-10-02 16:05:17 +0530

    added detailed comments

--- scripts/mk_gr_waveform_nsbh_sxs.py
+++ scripts/mk_gr_waveform_nsbh_sxs.py
@@ -44,24 +44,28 @@ for cbc in cbc_list:
 
 	print "... case:", cbc, iota, pol
 
+	# reading data
 	data_loc = glob.glob(data_dir + '/NRPolzns_%s_SpEC_q6.00_*_iota_%.2f_psi_%.2f.dat.gz'%(cbc, iota, pol))[0]
 	t_geom, hp_geom, hc_geom = np.loadtxt(data_loc, unpack=True)
 	print "... read data"
 
 	r = 1. #in Mpc
 
+	# converting from geometric to SI units for distance r = 1 Mpc
+	# and computing the evolution of phase phi(t) and instantaneous frequency F(t) = (1/2*pi)*(dphi/dt)
 	t_SI = t_geom * (M*lal.MTSUN_SI)
 	hp_SI = hp_geom*(M*lal.MTSUN_SI)/(r*1e6*lal.PC_SI/lal.C_SI)
 	hc_SI = hc_geom*(M*lal.MTSUN_SI)/(r*1e6*lal.PC_SI/lal.C_SI)
-
 	h_SI = hp_SI + 1j*hc_SI
 	phi_SI = np.unwrap(np.angle(h_SI))
 	Foft_SI = np.gradient(phi_SI)/np.gradient(t_SI)/(2*np.pi)
 
-	idx_flow15, = np.where(Foft_SI > 15.)
 
+	# restricting waveform to instantaneous frequencies above 15Hz
+	idx_flow15, = np.where(Foft_SI > 15.)
 	t_SI_flow15, hp_SI_flow15, hc_SI_flow15 = t_SI[idx_flow15], hp_SI[idx_flow15], hc_SI[idx_flow15]
 
+	# interpolating h_p(t) and h_c(t) over a equally-space time series with sampling rate 16kHz
 	t_SI_flow15_interp = np.arange(t_SI_flow15[0], t_SI_flow15[-1], 1/16384.)
 	dt_SI_flow15_interp = np.mean(np.diff(t_SI_flow15_interp))
         hp_SI_flow15_interp_obj = scipy.interpolate.interp1d(t_SI_flow15, hp_SI_flow15, fill_value=0., bounds_error=False)
@@ -69,6 +73,7 @@ for cbc in cbc_list:
         hp_SI_flow15_interp = hp_SI_flow15_interp_obj(t_SI_flow15_interp)
         hc_SI_flow15_interp = hc_SI_flow15_interp_obj(t_SI_flow15_interp)
 
+	# plotting waveform before and after interpolation (and Foft_SI)
 	plt.figure(figsize=(10,10))
         plt.subplot(311)
         plt.plot(t_SI_flow15, hp_SI_flow15, 'k', lw=0.2)
@@ -81,6 +86,7 @@ for cbc in cbc_list:
 	plt.savefig(out_dir + '/%s_interpolation.png'%out_file, dpi=300)
         plt.close()
 
+	# computing SNR for r = 1Mpc
 	N = len(hp_SI_flow15_interp)
 	f_SI = np.fft.fftfreq(N, d=dt_SI_flow15_interp)
 	df = np.diff(f_SI)[0]
@@ -93,23 +99,26 @@ for cbc in cbc_list:
 
 	print '... initial SNR:%f'%SNR
 
+	# rescaling (distance, hp, hc) for fixed SNR = 25
 	r = SNR/SNR_req
+        hp_SI_rescaled = hp_SI_flow15_interp/r
+        hc_SI_rescaled = hc_SI_flow15_interp/r
 
-        hp_SI_rescaled = hp_SI_flow15_interp/r#hp_geom_interp*(M*lal.MTSUN_SI)/(r*1e6*lal.PC_SI/lal.C_SI)
-        hc_SI_rescaled = hc_SI_flow15_interp/r#hc_geom_interp*(M*lal.MTSUN_SI)/(r*1e6*lal.PC_SI/lal.C_SI)
-
+	# sanity check: recomputing SNR for rescaled waveform (confirm SNR = 25)
 	signal=Fp*hp_SI_rescaled+Fc*hc_SI_rescaled
         signal_time=pycbc.types.timeseries.TimeSeries(signal,delta_t=dt_SI_flow15_interp,dtype=float)
         SNR=mfilter.sigma(signal_time,psd=psd,low_frequency_cutoff=f_low)
 
 	print '... rescaled distance: %f Mpc for a fixed SNR: %f'%(r, SNR)
 
+	# generate Fourier domain waveform
 	signal_freq = np.fft.fft(signal)*dt_SI_flow15_interp
 	data=signal_freq#+noise ## comment noise to generate noise free data
 
 	datar=np.real(data)
 	datai=np.imag(data)
 
+	# plotting Fourier data and PSD
 	plt.figure(figsize=(8,6))
 	plt.loglog(f_SI, abs(data), 'r')
 	plt.loglog(f_SI, psd**0.5, 'c')
@@ -118,6 +127,7 @@ for cbc in cbc_list:
 	plt.savefig(out_dir + '/%s_data.png'%out_file)
 	plt.close()
 
+	# saving data
 	np.savetxt(out_dir + '/%s_data.dat'%out_file, np.c_[f_SI[range(N/2)],datar[range(N/2)],datai[range(N/2)],psd[range(N/2)]], header='f real_data imag_data psd')
 	np.savetxt(out_dir + '/%s_initial.dat'%out_file, np.c_[Mc, q, Mc, q, r, iota, t0, Psi_ref, ra, np.sin(dec), pol], header='Mc q Mc1 q1 r iota t0 Psi_ref ra sin_dec pol')
 

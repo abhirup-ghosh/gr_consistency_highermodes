4742463bfd9b76d1ae4c419211197a10f020b6b5
abhirup.ghosh <abhirup.ghosh@ligo.org>
2018-11-01 22:29:27 +0530

    added read_emcee_samples.py

--- src/read_emcee_samples.py
+++ src/read_emcee_samples.py
@@ -1,11 +1,18 @@
 import numpy as np
 
-def read_emcee_samples(post_loc, nwalkers, num_iter, ndim, n_burnin):
+def read_emcee_samples_11dim(post_loc, nwalkers, num_iter, ndim, n_burnin):
 
 	samples_preburnin = np.loadtxt(post_loc).reshape(nwalkers, num_iter, ndim)
 	samples_postburnin = samples_preburnin[:,n_burnin:,:].reshape(-1,ndim)
 	mc, q, mc1, q1, dL, i, t0, phi0, ra, sin_dec, pol = samples_postburnin[:,0],samples_postburnin[:,1],samples_postburnin[:,2],samples_postburnin[:,3],samples_postburnin[:,4],samples_postburnin[:,5],samples_postburnin[:,6],samples_postburnin[:,7],samples_postburnin[:,8],samples_postburnin[:,9],samples_postburnin[:,10]
 	return mc, q, mc1, q1, dL, i, t0, phi0, ra, sin_dec, pol
+
+def read_emcee_samples_9dim(post_loc, nwalkers, num_iter, ndim, n_burnin):
+
+        samples_preburnin = np.loadtxt(post_loc).reshape(nwalkers, num_iter, ndim)
+        samples_postburnin = samples_preburnin[:,n_burnin:,:].reshape(-1,ndim)
+        mc, q, dL, i, t0, phi0, ra, sin_dec, pol = samples_postburnin[:,0],samples_postburnin[:,1],samples_postburnin[:,2],samples_postburnin[:,3],samples_postburnin[:,4],samples_postburnin[:,5],samples_postburnin[:,6],samples_postburnin[:,7],samples_postburnin[:,8]
+        return mc, q, dL, i, t0, phi0, ra, sin_dec, pol
 
 
 if __name__ == '__main__':
